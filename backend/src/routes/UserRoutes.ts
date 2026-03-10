9ae7013ffb3100e560663f1ed5ff0b1705043fc9
dudarmouras <dudarmouras@gmail.com>
2026-03-10 11:00:00 -0300

    feat/crud: user routes + testing

--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -3,7 +3,16 @@ import { UserController } from '../controller';
 
 const UserRoutes = Router();
 
-// Creating my first public route for the User creation
+// Create User
 UserRoutes.post('/', UserController.create);
+
+// Read User
+UserRoutes.get('/:id', UserController.read);
+
+// Update User
+UserRoutes.put('/:id', UserController.update);
+
+//Delete User
+UserRoutes.delete('/:id', UserController.delete);
 
 export default UserRoutes;
